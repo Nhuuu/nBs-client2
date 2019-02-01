632f666a40ff77171e056cb196bac24977da6842
Teffisk <brad.mcknight91@gmail.com>
2019-01-31 16:57:59 -0800

    cleaned up duplicate code in form

--- src/NewDrinkForm.js
+++ src/NewDrinkForm.js
@@ -26,6 +26,20 @@ class NewDrinkForm extends Component {
     })
   }
 
+  rerender = () => {
+    fetch(SERVER_URL+'/drinks')
+    .then(response => {
+      return response.json() 
+    })
+    .then(json => {
+      // console.log(json)
+      this.setState({ drinkItem: json })
+    })
+    .catch(err => {
+      console.log("Error fetching drinks!", err)
+    })   
+  }
+
   postDrink = (e) => {
     e.preventDefault()
     const user = JSON.stringify(this.props.user);
@@ -41,8 +55,8 @@ class NewDrinkForm extends Component {
 		.then(response => response.json())
 		.then(json => {
 			// console.log(json)
-      console.log('RUNNING FETCH', this.props)
-			this.props.rerender()
+      //console.log('RUNNING FETCH', this.props)
+			this.rerender()
 		})
 		.catch(err => {
 			console.log('Error posting data!', err)
@@ -91,43 +105,6 @@ class NewDrinkForm extends Component {
     else {
       return <p>Love yourself!</p>
     }
-
-    return(
-      <div>
-			  <form onSubmit={this.postDrink}>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Name</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="name" id="name" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Brand</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="brand"  id="brand" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Type</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="type"  id="type" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Sub-type</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="subtype"  id="subtype" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Price</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="number" name="price"  id="price" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Description</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="desc"  id="desc" onChange={this.storeInput} />
-          </div>
-          <div className="mt3">
-            <label className="db fw4 lh-copy f6">Image Url</label>
-            <input className="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="img"  id="img" onChange={this.storeInput} />
-          </div>
-          <input type="hidden" value={user.id} />
-          <div className="mt3"><input className="b mb5 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" type="submit" value="Add a Drink" /></div>
-        </form>
-      </div>
-    )
 	}
 }
 
