6d821283c065b79677437499327894e21412329a
Teffisk <brad.mcknight91@gmail.com>
2019-02-01 10:17:40 -0800

    added editDrink function with fetch to form

--- src/EditDrinkForm.js
+++ src/EditDrinkForm.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
 
 class EditDrinkForm extends Component {
+
+	editDrink = () => {
+		fetch(SERVER_URL+'/drinks/'+this.props.drink._id, {
+			method: "PUT"
+		})
+		.then(response => {
+			let responseJSON = response.status===204 ? {} : response.json()
+			return responseJSON
+		})
+		.then(json => {
+			this.props.rerender()
+		})
+		.catch(err => {
+			console.log("Error editing drink", err)			
+		})
+	}
+
 	render(){
 		return 
 	}
